8eabe22845baf5a85fd323391437e4cbff146b38
JunYoung Choi <cjy13753@gmail.com>
2021-11-06 14:04:04 +0900

    #9020, passed by making finding prime numbers only once

--- 9020/solution_9020.py
+++ 9020/solution_9020.py
@@ -1,35 +1,41 @@
+from collections import OrderedDict
+
 num_testcases = int(input())
 testcases = []
 for i in range(num_testcases):
     testcases.append(int(input()))
 
 def solution(testcases: list) -> None:
-    for testcase in testcases:
-        find_combination(testcase)
+    prime_num_list = OrderedDict()
+    prime_num_list[2] = None
 
-def find_combination(testcase: int) -> None:
-    prime_num_list = {2}
-    
-    for num in range(3, testcase):
+    for num in range(3, max(testcases)):
         is_prime = True
         for prime_num in prime_num_list:
             if (num % prime_num == 0):
                 is_prime = False
                 break
         if (is_prime == True):
-            prime_num_list.add(num)
-        
+            prime_num_list[num] = None
+    
+    for testcase in testcases:
+        find_combination(testcase, prime_num_list)
+
+def find_combination(testcase: int, prime_num_list: OrderedDict) -> None:
     possible_combination = ()
     smallest_difference = 10000
     for prime_num in prime_num_list:
+        if (prime_num > testcase):
+            break
+        
         other_prime_num = testcase - prime_num
         if other_prime_num in prime_num_list:
-            difference = abs(prime_num - other_prime_num)
+            difference = abs(other_prime_num - prime_num)
             if difference < smallest_difference:
                 smallest_difference = difference
                 possible_combination = (prime_num, other_prime_num)
     
-    print(f"{min(possible_combination)} {max(possible_combination)}")
+    print(f"{possible_combination[0]} {possible_combination[1]}")
     
 
 solution(testcases)
