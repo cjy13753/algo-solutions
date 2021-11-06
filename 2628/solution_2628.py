225de660d0ce42e0172071af5ce319c784ffe2e7
JunYoung Choi <cjy13753@gmail.com>
2021-11-06 16:38:12 +0900

    Change code more pythonic without changing underlying logic

--- 2628/solution_2628.py
+++ 2628/solution_2628.py
@@ -1,37 +1,16 @@
 width, height = map(int, input().split())
 num_cut = int(input())
-horizontal_cut_num = []
-vertical_cut_num = []
+horizontal = [0, height]
+vertical = [0, width]
 for i in range(num_cut):
-    cut = list(map(int, input().split()))
-    if (cut[0] == 0):
-        horizontal_cut_num.append(cut[1])
-    else:
-        vertical_cut_num.append(cut[1])
+    type, num = map(int, input().split())
+    (horizontal if type == 0 else vertical).append(num)
+horizontal.sort()
+vertical.sort()
 
-def solution(width: int, height: int, horizontal_cut_num: list, vertical_cut_num: list) -> None:
-    pieces_height = []
-    pieces_width = []
+def solution(horizontal: list, vertical: list) -> None:
+    max_height = max(horizontal[i + 1] - horizontal[i] for i in range(len(horizontal) - 1))
+    max_width = max(vertical[i + 1] - vertical[i] for i in range(len(vertical) - 1))
+    print(max_height * max_width)
 
-    if (len(horizontal_cut_num) > 0):
-        horizontal_cut_num.sort()
-        pieces_height.append(horizontal_cut_num[0])
-        for i in range(len(horizontal_cut_num) - 1):
-            pieces_height.append(horizontal_cut_num[i + 1] - horizontal_cut_num[i])
-        pieces_height.append(height - horizontal_cut_num[-1])
-    else:
-        pieces_height.append(height)
-    
-    if (len(vertical_cut_num) > 0):
-        vertical_cut_num.sort()
-        pieces_width.append(vertical_cut_num[0])
-        for i in range(len(vertical_cut_num) - 1):
-            pieces_width.append(vertical_cut_num[i + 1] - vertical_cut_num[i])
-        pieces_width.append(width - vertical_cut_num[-1])
-    else:
-        pieces_width.append(width)
-    
-    print(max(pieces_height) * max(pieces_width))
-    
-
-solution(width, height, horizontal_cut_num, vertical_cut_num)
+solution(horizontal, vertical)
