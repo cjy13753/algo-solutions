55b079938d0b981a8caf8603d599b9c220f23bb8
JunYoung Choi <cjy13753@gmail.com>
2021-11-08 21:59:00 +0900

    Fix typos and rearrange code for better readability in #1074

--- 1074/solution_1074.py
+++ 1074/solution_1074.py
@@ -1,5 +1,6 @@
 n, t_row, t_col = map(int, input().split())
 
+base_quad = [[0,1], [2,3]]
 def searchZ(n: int, t_row: int, t_col: int, base_quad: list) -> int:
     if n == 1:
         return base_quad[t_row][t_col]
@@ -10,10 +11,9 @@ def searchZ(n: int, t_row: int, t_col: int, base_quad: list) -> int:
     prev_quad_size =  pow(2, 2*(n - 1))
 
     base_quad_row = t_row//pow(2, n-1)
-    base_quad_row = t_col//pow(2, n-1)
+    base_quad_col = t_col//pow(2, n-1)
 
     return searchZ(n-1, t_prime_row, t_prime_col, base_quad) \
-            + prev_quad_size * base_quad[base_quad_row][base_quad_row]
+            + prev_quad_size * base_quad[base_quad_row][base_quad_col]
 
-base_quad = [[0,1], [2,3]]
 print(searchZ(n, t_row, t_col, base_quad))
