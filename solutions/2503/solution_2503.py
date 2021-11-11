503caf100c1b1c1947389c10a94c94008e45457a
JunYoung Choi <cjy13753@gmail.com>
2021-11-11 16:49:23 +0900

    Improve code readability for #2503 with the underlying logic intact

--- solutions/2503/solution_2503.py
+++ solutions/2503/solution_2503.py
@@ -1,40 +1,36 @@
 import sys
 from itertools import permutations
 
-def solution(guesess_and_answers: list) -> None:
+def solution(guesses_and_results: list) -> None:
     count = 0
-    for candidate in permutations([chr(ord('1') + i) for i in range(9)], 3):
-        flag = True
-        for guess_and_answer in guesess_and_answers:
-            guess_strike = guess_and_answer[1]
-            guess_ball = guess_and_answer[2]
-            candidite_strike_against_guess = 0
-            candidite_ball_against_guess = 0
-            guess = guess_and_answer[0]
+    candidates = permutations(list(range(1, 10)), 3)
 
-            for i in range(3):
-                if guess[i] == candidate[i]:
-                    candidite_strike_against_guess += 1
-                elif guess[i] in candidate:
-                    candidite_ball_against_guess += 1
-            
-            if guess_strike != candidite_strike_against_guess:
-                flag = False
-                break
-            
-            if guess_ball != candidite_ball_against_guess:
-                flag = False
-                break
-        
-        if flag == True:
+    for candidate in candidates:
+        if validate(candidate, guesses_and_results):
             count += 1
-    
+
     print(count)
 
+def validate(candidate, guesses_and_results):
+    for guess_and_result in guesses_and_results:
+        guess, guess_strike, guess_ball = [*guess_and_result]
+        candidite_strike_against_guess, candidite_ball_against_guess = 0, 0
+
+        for i in range(3):
+            if int(guess[i]) == candidate[i]:
+                candidite_strike_against_guess += 1
+            elif int(guess[i]) in candidate:
+                candidite_ball_against_guess += 1
+        
+        if guess_strike != candidite_strike_against_guess or guess_ball != candidite_ball_against_guess:
+            return False
+
+    return True
+
 n = int(sys.stdin.readline())
-guesses_and_answers = []
+guesses_and_results = []
 for _ in range(n):
     guess, strike, ball = sys.stdin.readline().split()
-    guesses_and_answers.append([list(guess), int(strike), int(ball)])
+    guesses_and_results.append((guess, int(strike), int(ball)))
 
-solution(guesses_and_answers)
+solution(guesses_and_results)
