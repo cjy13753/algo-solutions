ed05d42e194260c9cf828d60d2991e0ab3829f26
JunYoung Choi <cjy13753@gmail.com>
2021-11-13 16:24:17 +0900

    Remove code portion for memoization because there's no need in this problem

--- solutions/1629/solution_1629.py
+++ solutions/1629/solution_1629.py
@@ -1,21 +1,16 @@
 import sys
 
-def find_remn(num: int, power: int, div: int, cache: dict = {}) -> int:
+def find_remn(num: int, power: int, div: int) -> int:
     if power == 1:
-        res = num % div
-        cache[power] = res
-        return res
+        return num % div
     
-    first_half = power // 2
-    second_half = power - power // 2
+    remn = find_remn(num, power // 2, div)
 
-    first_half_remn = cache[first_half] if first_half in cache else find_remn(num, first_half, div, cache)
-    second_half_remn = cache[second_half] if second_half in cache is not None else find_remn(num, second_half, div, cache)
-
-    res = (first_half_remn * second_half_remn) % div
-    cache[power] = res
-
-    return res
+    if power % 2 == 1:
+        return remn ** 2 * num % div
+    else:
+        return remn ** 2 % div
+    
 
 num, power, div = list(map(int, sys.stdin.readline().split()))
 print(find_remn(num, power, div))
