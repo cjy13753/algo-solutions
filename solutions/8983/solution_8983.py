733fd91380e776fa8dcf510e2f78d0aaf3828f90
JunYoung Choi <cjy13753@gmail.com>
2021-11-13 13:03:24 +0900

    Remove unnecessary function calls in #8983

--- solutions/8983/solution_8983.py
+++ solutions/8983/solution_8983.py
@@ -1,41 +1,31 @@
 import sys
 
-def num_hunts(hunters_coordinates:list, birds_coordinates: list) -> None:
-    hunters_coordinates.sort()
+def num_hunts(hunters_x:list, birds_xy: list, gun_range: int) -> None:
+    hunters_x.sort()
 
     count = 0
-    for bird_coordinate in birds_coordinates:
-        bird_x_coordinate = bird_coordinate[0]
-        min_x_diff = find_min_diff(bird_x_coordinate, hunters_coordinates)
-        bird_y_coordindate = bird_coordinate[1]
-        if min_x_diff + bird_y_coordindate <= gun_range:
-            count += 1
+    for bird_xy in birds_xy:
+        bird_x, bird_y = bird_xy[0], bird_xy[1]
+        start = 0
+        end = len(hunters_x) - 1
+        while start <= end:
+            mid = start + (end - start) // 2
+            if abs(hunters_x[mid] - bird_x) + bird_y <= gun_range:
+                count += 1
+                break
 
+            if hunters_x[mid] > bird_x:
+                end = mid - 1
+            elif hunters_x[mid] == bird_x:
+                break
+            else:
+                start = mid + 1
     print(count)
 
-def find_min_diff(bird_x_coordinate: int, hunters_coordinates: list) -> int:
-    start = 0
-    end = len(hunters_coordinates) - 1
-    min_diff = float('inf')
+n_hunters, n_birds, gun_range = list(map(int, sys.stdin.readline().split()))
+hunters_x = list(map(int, sys.stdin.readline().split()))
+birds_xy = []
+for _ in range(n_birds):
+    birds_xy.append(list(map(int, sys.stdin.readline().split())))
 
-    while start <= end:
-        mid = start + (end - start) // 2
-        x_diff = hunters_coordinates[mid] - bird_x_coordinate
-        if x_diff > 0:
-            end = mid - 1
-            min_diff = min(min_diff, abs(x_diff))
-        elif x_diff == 0:
-            return x_diff
-        else:
-            start = mid + 1
-            min_diff = min(min_diff, abs(x_diff))
-    
-    return int(min_diff)
-
-num_hunters, num_birds, gun_range = list(map(int, sys.stdin.readline().split()))
-hunters_coordinates = list(map(int, sys.stdin.readline().split()))
-birds_coordinates = []
-for _ in range(num_birds):
-    birds_coordinates.append(list(map(int, sys.stdin.readline().split())))
-
-num_hunts(hunters_coordinates, birds_coordinates)
+num_hunts(hunters_x, birds_xy, gun_range)
