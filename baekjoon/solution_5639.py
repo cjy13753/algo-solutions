69dd988b579af8ac5b50a07e1c0d99ccdda53d98
JunYoung Choi <cjy13753@gmail.com>
2021-11-19 19:12:51 +0900

    #5639, remove list slicing and pass online judge

--- baekjoon/solution_5639.py
+++ baekjoon/solution_5639.py
@@ -1,16 +1,22 @@
 import sys
 sys.setrecursionlimit(10 ** 9)
 
-def makePostOrder(preorder) -> list:
-    length = len(preorder)
-    if length <= 1:
-        return preorder
-    a=preorder[0]
-    for i in range(1, length):
-        if preorder[i] > a:
-            return makePostOrder(preorder[1:i]) + makePostOrder(preorder[i:]) + [a]
-        i += 1
-    return makePostOrder(preorder[1:]) + [a]
+def convertToPostOrder(preorder: list, start: int, end: int) -> None:
+    if start > end:
+        return
+    
+    root = preorder[start]
+
+    idx = start + 1
+    while idx <= end:
+        if preorder[idx] > root:
+            break
+        idx += 1
+    
+    convertToPostOrder(preorder, start + 1, idx - 1)
+    convertToPostOrder(preorder, idx, end)
+    
+    print(root)
 
 preorder = []
 while True:
@@ -19,6 +25,4 @@ while True:
     except:
         break
 
-postorder = makePostOrder(preorder)
-for e in postorder:
-    print(e)
+convertToPostOrder(preorder, 0, len(preorder) - 1)
