6254a924708468c70ffa1586a2753d90fa320eed
JunYoung Choi <cjy13753@gmail.com>
2021-11-19 19:10:04 +0900

    #5639, failed because list slicing takes up too much time

--- baekjoon/solution_5639.py
+++ baekjoon/solution_5639.py
@@ -1,44 +1,24 @@
 import sys
 sys.setrecursionlimit(10 ** 9)
 
-class Node:
-    def __init__(self, key: int) -> None:
-        self.key = key
-        self.left = None
-        self.right = None
+def makePostOrder(preorder) -> list:
+    length = len(preorder)
+    if length <= 1:
+        return preorder
+    a=preorder[0]
+    for i in range(1, length):
+        if preorder[i] > a:
+            return makePostOrder(preorder[1:i]) + makePostOrder(preorder[i:]) + [a]
+        i += 1
+    return makePostOrder(preorder[1:]) + [a]
 
-def build(root: Node) -> None:
-    try:    
-        while True:
-            newNode = Node(int(input()))
-            prevNode = root
-            curNode = root
-            direction = 'L'
-            
-            while curNode is not None:
-                prevNode = curNode
-                if newNode.key < curNode.key:
-                    curNode = curNode.left
-                    direction = 'L'
-                else:
-                    curNode = curNode.right
-                    direction = 'R'
-            if direction == 'L':
-                prevNode.left = newNode
-            else:
-                prevNode.right = newNode
+preorder = []
+while True:
+    try:
+        preorder.append(int(sys.stdin.readline()))
     except:
-        return
+        break
 
-
-def postorder(root: Node) -> None:
-    if root is None:
-        return
-
-    postorder(root.left)
-    postorder(root.right)
-    print(root.key)
-
-root = Node(int(input()))
-build(root)
-postorder(root)
+postorder = makePostOrder(preorder)
+for e in postorder:
+    print(e)
