7a13c5615e388c841cdc2ac42d539fc882acf502
JunYoung Choi <cjy13753@gmail.com>
2021-11-20 17:09:29 +0900

    11725, succeeded using the hint of dfs starting from node 1

--- baekjoon/solution_11725.py
+++ baekjoon/solution_11725.py
@@ -9,31 +9,20 @@ if __name__ == '__main__':
         node1, node2 = map(int, input().split())
         tree[node1].append(node2)
         tree[node2].append(node1)
-    for i in range(2, numNodes + 1): # 각 노드에 대해서 parent를 구해준다
-        if len(tree[i]) == 1:
-            print(tree[i][0])
-            continue
-        if 1 in tree[i]:
-            print(1)
-            continue
-        parent = -1
-        for j in tree[i]: # 각 노드에 연결된 이웃 노드에 대해서 루트인 1로 이어지는 방향에 있는지 확인해준다.
-            visited = set()
-            stack = []
-            stack.append((j, j)) # (next, candidate)
-            flag = False
-            while stack:
-                next, candidate = stack.pop()
-                visited.add(next)
-                if next == 1:
-                    parent = candidate
-                    flag = True
-                    break
-                else:
-                    for next in tree[next]:
-                        if next not in visited:
-                            stack.append((next, candidate))
 
-            if flag == True:
-                break
-        print(parent)
+    parent = [0] * (numNodes + 1)
+
+    stack = []
+    visited = set()
+    stack.append(1)
+    parent[1] = 1
+    while stack:
+        popped = stack.pop()
+        visited.add(popped)
+        for i in tree[popped]:
+            if i not in visited:
+                parent[i] = popped
+                stack.append(i)
+    
+    for i in range(2, numNodes + 1):
+        print(parent[i])
