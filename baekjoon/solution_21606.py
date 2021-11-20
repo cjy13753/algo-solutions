029e9a5e5e97246c5c49337d3ed365be7be46187
JunYoung Choi <cjy13753@gmail.com>
2021-11-21 00:27:42 +0900

    21606: scored only 13 out of 200

--- baekjoon/solution_21606.py
+++ baekjoon/solution_21606.py
@@ -2,32 +2,28 @@ import sys
 input = sys.stdin.readline
 
 
-def calPaths(graph: list, col: list) -> int:
-    count = [0]
-    def dfs(vertex: int) -> None:
-        for i in graph[vertex]:
-            visited = set()
-            visited.add(vertex)
-            stack = []
-            stack.append(i)
+def calNearInterior(graph: list, col: list, exterior: list) -> int:
+    visitedExterior = set()
+    visitedInterior = set()
+    def dfs(exterior: int) -> int:
+        cnt = 0
+        for neighbor in graph[exterior]:
+            if col[neighbor] == 1:
+                if neighbor not in visitedInterior:
+                    visitedInterior.add(neighbor)
+                    cnt += 1
+            else:
+                if neighbor not in visitedExterior:
+                    visitedExterior.add(neighbor)
+                    cnt += dfs(neighbor)
+        return cnt
 
-            while stack:
-                popped = stack.pop()
-                visited.add(popped)
-                if col[popped] == 1:
-                    count[0] += 1
-                else:
-                    for j in graph[popped]:
-                        if j not in visited:
-                            stack.append(j)
-
-    for vertex in range(1, len(graph)):
-        if col[vertex] == 0:
-            continue
-        dfs(vertex)
-    
-    return count[0]
-
+    count = 0
+    for e in exterior:
+        visitedExterior.add(e)
+        numInterior = dfs(e)
+        count += numInterior * (numInterior - 1)
+    return count
 
 if __name__ == '__main__':
     numVertices = int(input())
@@ -39,4 +35,20 @@ if __name__ == '__main__':
         graph[v1].append(v2)
         graph[v2].append(v1)
 
-    print(calPaths(graph, col))
+    sum = 0
+    exterior = []
+    for i in range(1, numVertices + 1):
+        # 각 실내별 인접한 실내 구하기
+        if col[i] == 1:
+            for j in graph[i]:
+                if col[j] == 1:
+                    sum += 1
+        # 실외들만 따로 추리기
+        else:
+            exterior.append(i)
+
+    # 인접한 실외를 한 덩어리로 보고 그 덩어리에 인접한 실내의 수를 구한 뒤 각 덩어리별로 n*(n-1)의 경우의 수를 계산하고 모두 합친 값을 반환
+    n = calNearInterior(graph, col, exterior)
+    sum += n
+
+    print(sum)
