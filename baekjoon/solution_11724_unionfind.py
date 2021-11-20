b70c9a7f8b1ad78823fd4ec2427243fe4264b411
JunYoung Choi <cjy13753@gmail.com>
2021-11-20 12:59:58 +0900

    11724: passed test with unionfind approach but with bad memory and time usage

--- baekjoon/solution_11724_unionfind.py
+++ baekjoon/solution_11724_unionfind.py
@@ -24,10 +24,11 @@ if __name__ == '__main__':
     graph = []
     for _ in range(numEdges):
         src, dst = map(int, sys.stdin.readline().split())
+        if src > dst:
+            src, dst = dst, src
         graph.append([src, dst])
-    graph.sort() # 여기서 잘못 됐음. 입력 되는 데이터가 u < v일 것이라고 가정했기 때문. 하지만 (3, 4), (4, 2), (6, 2)와 같이 들어오면 문제 발생.
+    graph.sort()
     for src, dst in graph:
         unionFind(parent, src, dst)
 
-    print(parent)
     print(len(Counter(parent)) - 1)
