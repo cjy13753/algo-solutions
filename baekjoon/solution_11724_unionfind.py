e4ad4a7bb6b4608dea0d93f47c8e31ba4106149a
JunYoung Choi <cjy13753@gmail.com>
2021-11-20 14:05:38 +0900

    11724, union logic flaw leads to wrong answer

--- baekjoon/solution_11724_unionfind.py
+++ baekjoon/solution_11724_unionfind.py
@@ -8,27 +8,24 @@ def unionFind(parent: list, src: int, dst: int) -> None:
     if srcParent != dstParent:
         if srcParent < dstParent:
             parent[dst] = srcParent
-        else:
+        else:   
             parent[src] = dstParent
 
 def findParent(parent: list, vertex: int) -> int:
-    if parent[vertex] == vertex:
-        return vertex
-    
-    return findParent(parent, parent[vertex])
+    if parent[vertex] != vertex:
+        parent[vertex] = findParent(parent, parent[vertex])
+    return parent[vertex]
 
 if __name__ == '__main__':
     numVertices, numEdges = map(int, sys.stdin.readline().split())
     parent = [i for i in range(numVertices + 1)]
     
-    graph = []
     for _ in range(numEdges):
         src, dst = map(int, sys.stdin.readline().split())
-        if src > dst:
-            src, dst = dst, src
-        graph.append([src, dst])
-    graph.sort()
-    for src, dst in graph:
         unionFind(parent, src, dst)
+    
+    for i in range(1, numVertices + 1):
+        findParent(parent, i)
 
     print(len(Counter(parent)) - 1)
+    print(parent)
