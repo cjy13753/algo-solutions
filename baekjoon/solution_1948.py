df8019c732b0f5f5db1d032fe157d15ffb3a49f2
JunYoung Choi <cjy13753@gmail.com>
2021-11-24 22:15:03 +0900

    1948: Implement new logic from jinho's solution going forward and backward

--- baekjoon/solution_1948.py
+++ baekjoon/solution_1948.py
@@ -2,41 +2,36 @@ import sys
 from collections import defaultdict, deque
 input = sys.stdin.readline
 
-def solution(graph: defaultdict, indegree: dict, numCities: int, start: int, end: int):
-    itinerary = {i: [] for i in range(numCities + 1)} # dst: [[total hours, path]]
-    itinerary[start].append([0, [start]])
 
+def solution(indegree: dict, graph: defaultdict, graphBackward: defaultdict, start: int, end: int) -> None:
+    # forward
+    res = [0] * (numCities + 1)
+    
     queue = deque()
-    for i in range(1, numCities + 1):
-        if indegree[i] == 0:
-            queue.append(i)
-    
+    queue.append(start)
     while queue:
-        now = queue.popleft()
-
-        for dst, hour in graph[now]:
-            for totalHours, path in itinerary[now]:
-                totalHours += hour
-                newPath = []
-                for i in path:
-                    newPath.append(i)
-                newPath.append(dst)
-                itinerary[dst].append([totalHours, newPath])
-                indegree[dst] -= 1
-                if indegree[dst] == 0:
-                    queue.append(dst)
-
-    maxHours = int(-1e10)
-    possibleRoutes = set()
-    for candidate in itinerary[end]:
-        maxHours = max(maxHours, candidate[0])
-    for candidate in itinerary[end]:
-        if candidate[0] == maxHours:
-            for i in candidate[1]:
-                possibleRoutes.add(i)
+        src = queue.popleft()
+        for dst, hour in graph[src]:
+            indegree[dst] -= 1
+            res[dst] = max(res[dst], res[src] + hour)
+            if indegree[dst] == 0:
+                queue.append(dst)
     
-    print(maxHours)
-    print(len(possibleRoutes))
+    # backward
+    visited = [False] * (numCities + 1)
+    queue.append(end)
+    count = 0
+    while queue:
+        dst = queue.popleft()
+        for src, hour in graphBackward[dst]:
+            if res[dst] - res[src] == hour:
+                count += 1
+                if visited[src] == False:
+                    visited[src] = True
+                    queue.append(src)
+    
+    print(res[end])
+    print(count)
 
 
 if __name__ == '__main__':
@@ -46,11 +41,14 @@ if __name__ == '__main__':
     indegree = {i: 0 for i in range(1, numCities + 1)}
     
     graph = defaultdict(list)
+    graphBackward = defaultdict(list)
     for _ in range(numRoads):
         src, dst, hour = map(int, input().split())
         graph[src].append((dst, hour)) # graph: [dst, hour]
         indegree[dst] += 1
 
+        graphBackward[dst].append((src, hour))
+
     start, end = map(int, input().split())
 
-    solution(graph, indegree, numCities, start, end)
+    solution(indegree, graph, graphBackward, start, end)
