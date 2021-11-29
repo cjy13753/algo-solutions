f705d983246cb0e456adff4a004ad1bf4e7280fe
JunYoung Choi <cjy13753@gmail.com>
2021-11-29 14:27:59 +0900

    1946: got the answer but not very sure if I can use greedy approach in this problem

--- baekjoon/solution_1946.py
+++ baekjoon/solution_1946.py
@@ -14,16 +14,15 @@ class Solution:
 
 
     def maximumEmployees(self, candidates: list) -> None:
-        candidates.sort(key=lambda x: abs(x[1] - x[0]), reverse=True)
-        possible = []
-        for candidate in candidates:
-            flag = True
-            for i in possible:
-                if (candidate[0] < i[0] and candidate[1] < i[1]) or (candidate[0] > i[0] and candidate[1] > i[1]):
-                    flag = False
-                    break
-            if flag == True:
-                possible.append(candidate)
-        print(len(possible))
+        candidates.sort(key=lambda x: x[0])
+        cnt = 1
+        prevInterviewRank = candidates[0][1] # 서류 1등의 interviewScore
+        
+        for i in range(1, len(candidates)):
+            if candidates[i][1] < prevInterviewRank:
+                cnt += 1
+                prevInterviewRank = candidates[i][1]
+
+        print(cnt)
 
 Solution()
