333232fc5478333c448ef8810e30c8dcc10e5953
JunYoung Choi <cjy13753@gmail.com>
2021-11-30 23:44:44 +0900

    2253: Referred to jihoon's solution

--- baekjoon/solution_2253.py
+++ baekjoon/solution_2253.py
@@ -12,26 +12,27 @@ class Solution:
         self.minimumLeaps(numStones, smallRocks)
 
     def minimumLeaps(self, numStones: int, smallRocks: set):
-        dst = numStones 
-        # dp[prev][now]의 의미: prev에서 now로 왔을 때 목적지(dst)까지 필요한 최소 점프 수(만약 목적지 도착이 불가능하다면 INF)
-        dp = [[INF] * (numStones + 1) for _ in range(numStones + 1)]
+        maxJump = int((2 * numStones) ** (1/2)) + 1
 
-        def dfs(prev: int, now: int) -> int:
-            if dp[prev][now] != INF:
-                return dp[prev][now]
-            elif now == dst:
-                dp[prev][now] = 0
-                return 0
-            else:
-                prevLeap = now - prev
-                for leap in range(prevLeap - 1, prevLeap + 2):
-                    if leap >= 1 and now + leap <= numStones and (now + leap) not in smallRocks:
-                        dp[prev][now] = min(dp[prev][now], dfs(now, now + leap) + 1)
-                return dp[prev][now]
-        
-        if dfs(1, 2) == INF:
+        dp = [[INF] * (maxJump + 1) for _ in range(numStones + 1)]
+        dp[1][0] = 0
+
+        for stone in range(2, numStones + 1):
+            if stone in smallRocks:
+                continue
+            
+            currMaxSpeed = int((2 * stone - 2) ** (1/2))
+
+            for jump in range(1, currMaxSpeed + 1):
+                dp[stone][jump] = min(
+                    dp[stone - jump][jump - 1], 
+                    dp[stone - jump][jump], 
+                    dp[stone - jump][jump + 1]) + 1
+
+        minJump = min(dp[numStones])
+        if minJump == INF:
             print(-1)
         else:
-            print(dp[1][2] + 1)
+            print(minJump)
 
 Solution()
