3944512bd1a806f6bea628af106e218e8a1da15b
JunYoung Choi <cjy13753@gmail.com>
2021-11-30 15:29:30 +0900

    1700: solved with greedy approach

--- baekjoon/solution_1700.py
+++ baekjoon/solution_1700.py
@@ -1,32 +1,41 @@
 import sys
 input = sys.stdin.readline
+INF = sys.maxsize
 
 class Solution:
     def __init__(self) -> None:
         tabs, numItems = map(int, input().split())
         items = list(map(int, input().split()))
 
-        self.minimumPlugouts(tabs, items)
+        self.minimumPlugouts(tabs, items, numItems)
 
-    def minimumPlugouts(self, tabs:int, items: list) -> None:
-        plugged = set()
+    def minimumPlugouts(self, tabs:int, items: list, numItems: int) -> None:
+        pluggedIns = set()
+
         cnt = 0
         for i in range(len(items)):
-            if len(plugged) < tabs:
-                plugged.add(items[i])
+            if len(pluggedIns) < tabs:
+                pluggedIns.add(items[i])
             else:
-                if items[i] in plugged:
+                if items[i] in pluggedIns:
                     continue
                 else:
-                    notToBeUsed = plugged - set(items[i + 1:])
-                    if len(notToBeUsed) == 0:
-                        plugged.pop()
-                        cnt += 1
-                        plugged.add(items[i])
-                    else:
-                        plugged.remove(notToBeUsed.pop())
-                        cnt += 1
-                        plugged.add(items[i])
+                    maxIdx = -1
+                    itemAtMaxIdx = -1
+                    remainingList = items[i + 1:]
+                    for item in pluggedIns:
+                        idx = remainingList.index(item) if item in remainingList else INF
+                        if idx == INF:
+                            itemAtMaxIdx = item
+                            break
+                        else:
+                            if idx > maxIdx:
+                                maxIdx = idx
+                                itemAtMaxIdx = item
+                    pluggedIns.remove(itemAtMaxIdx)
+                    cnt += 1
+                    pluggedIns.add(items[i])
+
         print(cnt)
 
 Solution()
