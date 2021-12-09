2e0fb6d753865f5ea7f02cd01a5c1f96605ec938
JunYoung Choi <cjy13753@gmail.com>
2021-12-09 21:09:17 +0900

    leetcode 5: solved with dp and two pointers

--- leetcode/solution_5.py
+++ leetcode/solution_5.py
@@ -9,37 +9,21 @@ class Solution:
     def longestPalindrome(self, s: str) -> str:
         length = len(s)
         dp = [[False] * length for _ in range(length)]
-        for left in range(length):
-            dp[left][left] = True
+        for i in range(length):
+            dp[i][i] = True
+        longestPalindromicStart = 0
         maxPalinLength = 1
-        ans = [0, 0] # [left idx, right idx]
 
-        # recur determines if s[left:right + 1] is palindromic
-        def recur(left, right):
-            if dp[left][right] == True:
-                return True
-            
-            if left + 1 <= right - 1:
-                if s[left] == s[right] and recur(left + 1, right - 1) == True:
-                    dp[left][right] = True
-                    return True
-                else:
-                    return False
-            else:
-                if s[left] == s[right]:
-                    dp[left][right] = True
-                    return True
-                else:
-                    return False
+        for end in range(length):
+            for start in range(end - 1, -1, -1):
+                if end - start == 1 or dp[start + 1][end - 1] == True:
+                    if s[start] == s[end]:
+                        dp[start][end] = True
+                        if end - start + 1 > maxPalinLength:
+                            longestPalindromicStart = start
+                            maxPalinLength = end - start + 1
 
-        for left in range(length):
-            for right in range(left, length):
-                if s[left] == s[right] and recur(left, right):
-                    if right - left + 1 > maxPalinLength:
-                        maxPalinLength = right - left + 1
-                        ans[0], ans[1] = left, right
-
-        return s[ans[0]:ans[1] + 1]
+        return s[longestPalindromicStart:longestPalindromicStart + maxPalinLength]
 
 
 Solution()
