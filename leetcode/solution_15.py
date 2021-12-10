6c4b8b138a82e268c734aa7c87648a97f8b187c6
JunYoung Choi <cjy13753@gmail.com>
2021-12-10 13:05:47 +0900

    leetcode 15: referred to discussion's solution

--- leetcode/solution_15.py
+++ leetcode/solution_15.py
@@ -5,60 +5,39 @@ input = sys.stdin.readline
 
 class Solution:
     def __init__(self) -> None:
-        nums = [-2,0,1,1,2]
+        nums = [-4,-2,1,-5,-4,-4,4,-2,0,4,0,-2,3,1,-5,0]
         print(self.threeSum(nums))
 
-    # 정렬을 한다.(최종 결과값이 index의 모음이 아니기 때문에 정렬이 가능)
-    # left, right idx pointer를 잡아두고, [left + 1, right - 1] inclusive 범위에서 이분탐색해서 -(nums[left] + nums[right])의 값과 일치하는 값이 있는지 확인
-    # 값이 존재하면 그때의 값들의 조합을 set 자료구조에 넣어둔다.(duplicate triplet 방지 위함)
-    # nums[left] + nums[right]의 값이 음수면 left++, 양수면 right--
-    # 약간의 최적화를 위해서 left == prevLeft, right == prevRight이면 pass한다.
-    # 시간 복잡도 O(nlogn), 공간 복잡도 O(1)
     def threeSum(self, nums: List[int]) -> List[List[int]]:
+        ans = set()
+        
         if len(nums) < 3:
             return []
+
         nums.sort()
-        leftIdx = 0
-        rightIdx = len(nums) - 1
-        queue = deque()
-        queue.append((leftIdx, rightIdx))
-        ans = set()
+        for i in range(len(nums) - 2):
+            if i > 0 and nums[i] == nums[i - 1]:
+                continue
+            left = i + 1
+            right = len(nums) - 1
 
-        def binarySearch(leftIdx: int, rightIdx: int, twoSum: int) -> bool:
-            if leftIdx > rightIdx:
-                return False
-            
-            while leftIdx <= rightIdx:
-                mid = leftIdx + (rightIdx - leftIdx) // 2
-                if nums[mid] > twoSum:
-                    rightIdx = mid - 1
-                    continue
-                if nums[mid] == twoSum:
-                    return True
+            while left < right:
+                sum = nums[i] + nums[left] + nums[right]
+                if sum == 0:
+                    ans.add((nums[i], nums[left], nums[right]))
+                
+                    while left + 1 < len(nums) and nums[left] == nums[left + 1]:
+                        left += 1
+                    while right - 1 > -1 and nums[right] == nums[right - 1]:
+                        right -= 1
+                    
+                    left += 1
+                    right -= 1
+
+                elif sum < 0:
+                    left += 1
                 else:
-                    leftIdx = mid + 1
-                    continue
-            return False
-
-        visited = set()
-        while queue:
-            leftIdx, rightIdx = queue.popleft()
-            visited.add((leftIdx, rightIdx))
-            
-            twoSum = nums[leftIdx] + nums[rightIdx]
-            if binarySearch(leftIdx + 1, rightIdx - 1, -twoSum):
-                ans.add((nums[leftIdx], -twoSum, nums[rightIdx]))
-            if twoSum > 0:
-                if (leftIdx, rightIdx - 1) not in visited and leftIdx < rightIdx - 1:
-                    queue.append((leftIdx, rightIdx - 1))
-            elif twoSum == 0:
-                if (leftIdx, rightIdx - 1) not in visited and leftIdx < rightIdx - 1:
-                    queue.append((leftIdx, rightIdx - 1))
-                if (leftIdx + 1, rightIdx) not in visited and leftIdx + 1 < rightIdx:
-                    queue.append((leftIdx + 1, rightIdx))
-            else:
-                if (leftIdx + 1, rightIdx) not in visited and leftIdx + 1 < rightIdx:
-                    queue.append((leftIdx + 1, rightIdx))
+                    right -= 1
 
         return list(ans)
 
