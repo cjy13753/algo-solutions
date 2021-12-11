04a2b601f7332c154bb5a914f02c2c8cc1698108
JunYoung Choi <cjy13753@gmail.com>
2021-12-11 12:06:13 +0900

    leetcode 238: 2nd attempt by referring to leetcode discussion's solution

--- leetcode/solution_238.py
+++ leetcode/solution_238.py
@@ -2,31 +2,28 @@ import sys
 from typing import List
 input = sys.stdin.readline
 
-# initial attempt, took 40 minutes
-# approach keywords: two pointers with prefix product array and suffix product array
-# time complexity: O(n), 248 ms
-# space complexity: O(n), 22.4 MB
+# 2nd attempt: referred to leetcode user hqpdash's answer
+# memo: use special tactic to make space complexity more efficient from O(n) to O(1)
+# time complexity: O(n), 236 ms
+# space complexity: O(1), 21.2 MB
 
 class Solution:
     def __init__(self) -> None:
-        nums = [-1,1,0,-3,3]
+        nums = [2, 3, 4, 5]
         print(self.productExceptSelf(nums))
 
     def productExceptSelf(self, nums: List[int]) -> List[int]:
-        prefix = [0] * len(nums)
-        prefix[0] = 1
-        for i in range(1, len(nums)):
-            prefix[i] = prefix[i-1] * nums[i - 1]
-
-        suffix = [0] * len(nums)
-        suffix[-1] = 1
-        for i in range(len(nums) - 2, -1, -1):
-            suffix[i] = suffix[i+1] * nums[i + 1]
-
         ans = [0] * len(nums)
+        tmp = 1
         for i in range(len(nums)):
-            ans[i] = prefix[i] * suffix[i]
+            ans[i] = tmp
+            tmp = ans[i] * nums[i]
 
+        tmp = 1
+        for i in range(len(nums) - 1, -1 , -1):
+            ans[i] = ans[i] * tmp
+            tmp = tmp * nums[i]
+        
         return ans
 
 Solution()
