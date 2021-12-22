0da1a6c816d64cfb935baba78f1dca1758dd029a
JunYoung Choi <cjy13753@gmail.com>
2021-12-22 17:00:06 +0900

    leetcode 347: refer to leetcode solution using bucket sort

--- leetcode/solution_347.py
+++ leetcode/solution_347.py
@@ -1,37 +1,34 @@
 '''
 Summary
 
-Attempt #1
-Your own answer?: Yes
-time spent: 15m
-Time complexity: O(nlogn) because of sorting
-Runtime: 96 ms, faster than 88.92% of Python3 online submissions for Top K Frequent Elements.
+Attempt #2
+Your own answer?: No
+Reference: https://leetcode.com/problems/top-k-frequent-elements/discuss/740374/Python-5-lines-O(n)-buckets-solution-explained.
+Time complexity: O(n) because of bucket sort
+Runtime: Runtime: 108 ms, faster than 37.69% of Python3 online submissions for Top K Frequent Elements.
+Runtime analysis: I think the reason why the actual runtime for O(n) bucket sort is slower than my original O(nlogn) solution is 
+    probably because the given nums array size is limited to maximum 10^5.
 Space complexity: O(n)
-Memory Usage: 18.8 MB, less than 39.10% of Python3 online submissions for Top K Frequent Elements.
+Memory Usage: Memory Usage: 19.9 MB, less than 6.61% of Python3 online submissions for Top K Frequent Elements.
 '''
 
 import sys
-from collections import defaultdict
+from collections import Counter
+from itertools import chain
 from typing import List
 input = sys.stdin.readline
 
 class Solution:
     def __init__(self) -> None:
-        nums = [1]
-        print(self.topKFrequent(nums, 1))
+        nums = [1,1,1,2,2,3]
+        print(self.topKFrequent(nums, 2))
 
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
-        nums_freq = defaultdict(int)
-        for num in nums:
-            nums_freq[num] += 1
-
-        dict_items = list(nums_freq.items())
-        dict_items.sort(key=lambda x:x[1], reverse=True)
-
-        ans = []
-        for i in range(k):
-            ans.append(dict_items[i][0])
-
-        return ans
+        buckets = [[] for _ in range(len(nums) + 1)]
+        counter = Counter(nums)
+        for num, freq in counter.items():
+            buckets[freq].append(num)
+        flattened = list(chain(*buckets))
+        return flattened[::-1][:k]
 
 Solution()
