3fe158948513b35d62bec43106b29c797054f190
JunYoung Choi <cjy13753@gmail.com>
2021-12-29 21:06:32 +0900

    leetcode 78: different approach referring to leetcode discussion

--- leetcode/solution_78.py
+++ leetcode/solution_78.py
@@ -1,13 +1,10 @@
 '''
 Summary
 
-Attempt #1
-Your own answer?: Yes
-Time spent: 27m
+Attempt #2
+Your own answer?: No(https://leetcode.com/problems/subsets/discuss/122645/3ms-easiest-solution-no-backtracking-no-bit-manipulation-no-dfs-no-bullshit)
 
-Time Complexity: O(nC0 + nC1 + nC2 + ... + nCn)
-Runtime: 62 ms, faster than 5.45% of Python3 online submissions for Subsets.
-Space Complexity: O(N)
+Runtime: 39 ms, faster than 20.08% of Python3 online submissions for Subsets.
 Memory Usage: 14.5 MB, less than 18.52% of Python3 online submissions for Subsets.
 '''
 
@@ -21,23 +18,14 @@ class Solution:
         print(self.subsets(nums))
 
     def subsets(self, nums: List[int]) -> List[List[int]]:
-        ans = []
-        
-        def dfs(curr: int, path: list):
-            if curr == len(nums):
-                return
+        ans = [[]]
 
-            if curr == -1:
-                ans.append([])
-                for i in range(curr + 1, len(nums)):
-                    dfs(i, path + [])
-            else:
-                ans.append(path + [nums[curr]])
+        for n in nums:
+            tmp_ans = []
+            for subset in ans:
+                tmp_ans.append(subset + [n])
+            ans += tmp_ans
 
-                for i in range(curr + 1, len(nums)):
-                    dfs(i, path + [nums[curr]])
-
-        dfs(-1, [])
         return ans
 
 
