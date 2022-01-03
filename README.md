e46a0adaf2300e8bdb35e2b8ed50fe548ddb76b3
JunYoung Choi <cjy13753@gmail.com>
2022-01-03 20:20:04 +0900

    leetcode 207: solved with leetcode discussion solution

--- README.md
+++ README.md
@@ -1,3 +1,10 @@
 # Solutions to algorithm problems in [baekjoon](https://www.acmicpc.net/problemset) and [leetcode](https://leetcode.com/problemset/all/)
 
 For python code snippet generation in VS Code, refer to this [codepen work](http://codepen.io/mrmlnc/pen/GqrqPg) to make template code into multiple strings separated by comma. This needs sign-up for the website.
+
+# Time complexity brief reminder
+- Topological Sort w/ BFS
+    - Time Complexity
+        - O(V + E) because you loop through all vertices to find ones with 0 indegree and then remove edges extending from 0 indgree nodes.
+    - Space Complexity
+        - O(V) where V is the number of vertices
--- leetcode/solution_207.py
+++ leetcode/solution_207.py
@@ -1,11 +1,17 @@
 '''
 Summary
 
-Attempt #1
-Status: Failed - "Wrong Answer"
-Approach: BFS (if there is a cycle, return False)
-'''
+Attempt #2
+Your own answer?: No
+Reference: https://leetcode.com/problems/course-schedule/discuss/58509/C%2B%2B-BFSDFS
+Basic approach: Topological sort
 
+Time Complexity: O(V + E) because you loop through all vertices to find ones with 0 indegree 
+    and then remove edges extending from 0 indgree node.
+Runtime: 96 ms, faster than 78.18% of Python3 online submissions for Course Schedule.
+Space Complexity: O(V) where V is the number of courses
+Memory Usage: 15.5 MB, less than 85.37% of Python3 online submissions for Course Schedule.
+'''
 
 import sys
 from typing import List
@@ -20,29 +26,22 @@ class Solution:
 
     def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
         graph = {src: [] for src in range(numCourses)}
+        degree = [0] * numCourses
         for prereq in prerequisites:
             dst, src = prereq
             graph[src].append(dst)
+            degree[dst] += 1
+
+        queue = deque([course for course in range(numCourses) if degree[course] == 0])
+
+        while queue:
+            src = queue.popleft()
+
+            for dst in graph[src]:
+                degree[dst] -= 1
+                if degree[dst] == 0:
+                    queue.append(dst) 
+
+        return sum(degree) == 0
         
-        visited = [False] * numCourses
-        
-        for i in range(numCourses):
-            if not graph[i]:
-                continue
-            
-            queue = deque()
-            queue.append(i)
-
-            while queue:
-                popped = queue.popleft()
-                visited[i] = True
-
-                for dst in graph[popped]:
-                    if visited[dst] == True:
-                        return False
-                    else:
-                        queue.append(dst)
-
-        return True
-
 Solution()
