37baded6dc1b648137d2b7e16d3867d9241b766d
JunYoung Choi <cjy13753@gmail.com>
2022-01-04 14:47:44 +0900

    leetcode 743: simple dijkstra algorithm

--- leetcode/solution_743.py
+++ leetcode/solution_743.py
@@ -1,5 +1,17 @@
+'''
+Summary - Attempt #2
+
+Runtime: 957 ms, faster than 10.38% of Python3 online submissions for Network Delay Time.
+Memory Usage: 16.4 MB, less than 25.06% of Python3 online submissions for Network Delay Time.
+
+Basic approach: dijkstra with priority queue
+'''
+
+
 import sys
 from typing import List
+from collections import defaultdict
+import heapq
 input = sys.stdin.readline
 
 class Solution:
@@ -11,31 +23,34 @@ class Solution:
         print(self.networkDelayTime(times, n, k))
 
     def networkDelayTime(self, times: List[List[int]], n: int, k: int) -> int:
-        graph = [[0] * (n + 1) for _ in range(n + 1)]
-        for time in times:
-            src, dst, cost = time
-            graph[src][dst] = cost
+        INF = 1e10
+        adj_list = defaultdict(list) # adj_list = {src: [(dst, cost), ...]}
+        for src, dst, cost in times:
+            adj_list[src].append((dst, cost))
+        distance = {} # {dst: shorted distance up to dst}
+        visited = {}
+        for i in range(1, n + 1):
+            distance[i] = INF
+            visited[i] = False
 
-        maxTime = [float('inf')]
-        visited = [0] * (n + 1)
+        minheap = []
+        distance[k] = 0
+        heapq.heappush(minheap, (0, k))
 
-        def dfs(src, accum):
-            visited[src] = 1
-            
-            if sum(graph[src]) == 0:
-                maxTime[0] = min(maxTime[0], accum)
-                return
+        while minheap:
+            cost, src = heapq.heappop(minheap)
 
-            for dst, cost in enumerate(graph[src]):
-                if cost > 0:
-                    if visited[dst] == 1:
-                        maxTime[0] = min(maxTime[0], accum)
-                    else:
-                        dfs(dst, accum + cost)
+            if visited[src] == True:
+                continue
 
-        dfs(k,0)
-        
-        return maxTime[0] if sum(visited) == n else -1
+            visited[src] = True
 
+            for dst, newCost in adj_list[src]:
+                accumCost = cost + newCost
+                if accumCost < distance[dst]:
+                    distance[dst] = accumCost
+                    heapq.heappush(minheap, (accumCost, dst))
+
+        return max(distance.values()) if sum(visited.values()) == n else -1
 
 Solution()
