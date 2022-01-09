4f6883b3d9e03fd587527a84fb5f02063ef3c853
JunYoung Choi <cjy13753@gmail.com>
2022-01-09 20:17:20 +0900

    leetcode 105: passed with attempt #2

--- leetcode/solution_105.py
+++ leetcode/solution_105.py
@@ -1,7 +1,12 @@
 '''
-Summary - Attempt #1
+Summary - Attempt #2
 
-Fail due to wrong answer
+Your own answer?: No
+Reference: https://leetcode.com/problems/construct-binary-tree-from-preorder-and-inorder-traversal/discuss/981152/Recursion-or-Explanation-%2B-Visuals-or-Python
+Runtime: 291 ms, faster than 16.49% of Python3 online submissions for Construct Binary Tree from Preorder and Inorder Traversal.
+Memory Usage: 88.5 MB, less than 10.83% of Python3 online submissions for Construct Binary Tree from Preorder and Inorder Traversal.
+
+Memo: It's almost the same answer as my previous attempt. Only omitted 'recur' wrapper function, and it just worked. Don't know why.
 '''
 
 
@@ -18,31 +23,26 @@ class TreeNode:
 
 class Solution:
     def __init__(self) -> None:
-        pass
+        preorder = [3,9,20,15,7]
+        inorder = [9,3,15,20,7]
+        self.buildTree(preorder, inorder)
 
     def buildTree(self, preorder: List[int], inorder: List[int]) -> Optional[TreeNode]:
-        def recur(sliced_preorder, sliced_inorder):
-            if len(sliced_preorder) < 1:
-                return
-            
-            root_val = sliced_preorder[0]
-            root = TreeNode(root_val)
+        if len(preorder) < 1:
+            return
+        
+        root_val = preorder[0]
+        root = TreeNode(root_val)
 
-            if len(sliced_preorder) == 1:
-                return root
-
-            root_idx_for_inorder = inorder.index(root_val)
-            left_subtree_size = len(inorder[:root_idx_for_inorder])
-            
-            root.left = recur(sliced_preorder[1:left_subtree_size + 1], sliced_inorder[:root_idx_for_inorder])
-            root.right = recur(sliced_preorder[left_subtree_size + 1:], sliced_inorder[root_idx_for_inorder + 1:])
-
+        if len(preorder) == 1:
             return root
 
+        root_idx_for_inorder = inorder.index(root_val)
+        left_subtree_size = len(inorder[:root_idx_for_inorder])
+        
+        root.left = self.buildTree(preorder[1:left_subtree_size + 1], inorder[:root_idx_for_inorder])
+        root.right = self.buildTree(preorder[left_subtree_size + 1:], inorder[root_idx_for_inorder + 1:])
 
-        return recur(preorder, inorder)
-
-
-
+        return root
 
 Solution()
