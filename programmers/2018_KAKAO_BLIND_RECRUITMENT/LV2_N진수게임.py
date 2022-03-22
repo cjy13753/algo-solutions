737f24bee0d04aaa468268bf0e51a9488412931b
JunYoung Choi <cjy13753@gmail.com>
2022-03-22 14:02:51 +0900

    deque와 divmod 사용하여 코드 가독성 개선

--- programmers/2018_KAKAO_BLIND_RECRUITMENT/LV2_N진수게임.py
+++ programmers/2018_KAKAO_BLIND_RECRUITMENT/LV2_N진수게임.py
@@ -3,32 +3,34 @@ Attempt #1
 성공, 49m 소요
 """
 
+from collections import deque
+
+def convertToAlphabet(num):
+    return ['A', 'B', 'C', 'D', 'E', 'F'][num - 10]
+
+def numberToBase(num, base):
+    if num == 0:
+        return '0'
+    digits = deque()
+    while num:
+        q, r = divmod(num, base)
+        if r >= 10:
+            r = convertToAlphabet(r)
+        digits.appendleft(str(r))
+        num = q
+    return ''.join(digits)
+
 def solution(n, t, m, p):
-    def convertToAlphabet(num):
-        return ['A', 'B', 'C', 'D', 'E', 'F'][num - 10]
-    
-    def numberToBase(num):
-        if num == 0:
-            return '0'
-        digits = []
-        while num:
-            digit = int(num % n)
-            if digit >= 10:
-                digit = convertToAlphabet(digit)
-            digits.append(str(digit))
-            num //= n
-        return ''.join(digits[::-1])
-    
     fullString = ''
     num = 0
     while len(fullString) < m * (t - 1) + p:
-        fullString += numberToBase(num)
+        fullString += numberToBase(num, n)
         num += 1
     
     answer = ''
     count = 0
     i =  p - 1
-    while count != t:
+    while count < t:
         count += 1
         answer += fullString[i]
         i += m
