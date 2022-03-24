bb1633acdd234eb6af14e9b899656f0535ceb58f
JunYoung Choi <cjy13753@gmail.com>
2022-03-24 12:46:04 +0900

    Time Limit Exceeded

--- leetcode/practice-after-a-while/practice_solution_332.py
+++ leetcode/practice-after-a-while/practice_solution_332.py
@@ -2,38 +2,36 @@ from typing import List
 
 class Solution:
     def findItinerary(self, tickets: List[List[str]]) -> List[str]:
-        answer = []
+        answer = [[]]
         itinerary = []
         numRemainingTickets = [len(tickets)]
         adjList = {}
         for departCity, arriveCity in tickets:
             if departCity not in adjList:
-                adjList[departCity] = set()
-            if arriveCity not in adjList:
-                adjList[arriveCity] = set()
-            adjList[departCity].add(arriveCity)
+                adjList[departCity] = []
+            adjList[departCity].append(arriveCity)
 
-        def recur(departCity):
+        def dfs(departCity):
             itinerary.append(departCity)
             
-            if len(adjList[departCity]) == 0:
+            if departCity not in adjList or len(adjList[departCity]) == 0:
                 if numRemainingTickets[0] == 0:
-                    if len(answer) == 0:
-                        answer = itinerary
+                    if len(answer[0]) == 0:
+                        answer[0] = itinerary[:]
                     else:
-                        if ''.join(itinerary) < ''.join(answer):
-                            answer = itinerary
+                        if ''.join(itinerary) < ''.join(answer[0]):
+                            answer[0] = itinerary[:]
                 return
 
             for arriveCity in list(adjList[departCity]):
                 adjList[departCity].remove(arriveCity)
                 numRemainingTickets[0] -= 1
-                recur(arriveCity)
+                dfs(arriveCity)
                 itinerary.pop()
-                adjList[departCity].add(arriveCity)
+                adjList[departCity].append(arriveCity)
                 numRemainingTickets[0] += 1
 
-        recur("JFK")
-        return answer
+        dfs("JFK")
+        return answer[0]
 
-print(Solution().findItinerary([["MUC","LHR"],["JFK","MUC"],["SFO","SJC"],["LHR","SFO"]]))
+print(Solution().findItinerary([["EZE","AXA"],["TIA","ANU"],["ANU","JFK"],["JFK","ANU"],["ANU","EZE"],["TIA","ANU"],["AXA","TIA"],["TIA","JFK"],["ANU","TIA"],["JFK","TIA"]]))
