afccbdc0cd2384db3c15373fd45f3be5b6ffb9cc
JunYoung Choi <cjy13753@gmail.com>
2022-03-24 17:22:03 +0900

    Second attemp failed due to the same reason as the previous attempt

--- leetcode/solution_424.py
+++ leetcode/solution_424.py
@@ -1,47 +1,28 @@
 '''
-Summary - Attempt #1
+Summary - Attempt #2
 
-Time spent: 60m
-Status: Failt due to wrong answer
+Time spent: 50m
+Status: Failed due to wrong answer
 failed case: s="ABBB", k=2
 '''
 
-import sys
-from collections import deque
-input = sys.stdin.readline
+from collections import defaultdict
 
 class Solution:
-    def __init__(self) -> None:
-        s = "AABABBA"
-        k = 1
-        print(self.characterReplacement(s, k))
-
     def characterReplacement(self, s: str, k: int) -> int:
-        if s == 1:
-            return 1
-        
-        longest_windows_size = 0
-        queue = deque()
-        queue.append(0)
+        counter = defaultdict(int)
+        start_idx = 0
+        end_idx = 0
+        max_size = 0
 
-        while queue:
-            diff_char_num = 0
-            i = queue[0] + 1
-            while i < len(s):
-                if s[i] != s[queue[0]]:
-                    if diff_char_num < k:
-                        queue.append(i)
-                        diff_char_num += 1
-                    else:
-                        queue.append(i)
-                        break
-                i += 1
-            
-            if i - queue[0] > longest_windows_size:
-                longest_windows_size = i - queue[0]
-            
-            queue.popleft()
-        
-        return longest_windows_size
-            
-Solution()
+        while end_idx < len(s):
+            counter[s[end_idx]] += 1
+            if (end_idx - start_idx + 1) - counter[s[start_idx]] <= k:
+                max_size = max(max_size, end_idx - start_idx + 1)
+                end_idx += 1
+            else:
+                counter[s[end_idx]] -= 1
+                counter[s[start_idx]] -= 1
+                start_idx += 1
+
+        return max_size
