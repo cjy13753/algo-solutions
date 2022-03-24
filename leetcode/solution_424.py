a0129c17eaf7b60dffa2bb7159c601d85237f1d3
JunYoung Choi <cjy13753@gmail.com>
2022-03-24 18:06:35 +0900

    PASS: good for future reference for review

--- leetcode/solution_424.py
+++ leetcode/solution_424.py
@@ -1,9 +1,11 @@
 '''
-Summary - Attempt #2
+Summary - Attempt #3 (PASS)
 
-Time spent: 50m
-Status: Failed due to wrong answer
-failed case: s="ABBB", k=2
+Slight change to the previous attempt after referring to leetcode discussion.
+The key is to subtract the frequency of the most frequent charact in the window of the from the window size.
+
+Runtime: 146 ms, faster than 76.11% of Python3 online submissions for Longest Repeating Character Replacement.
+Memory Usage: 14 MB, less than 64.50% of Python3 online submissions for Longest Repeating Character Replacement.
 '''
 
 from collections import defaultdict
@@ -17,12 +19,12 @@ class Solution:
 
         while end_idx < len(s):
             counter[s[end_idx]] += 1
-            if (end_idx - start_idx + 1) - counter[s[start_idx]] <= k:
+            if (end_idx - start_idx + 1) - max(counter.values()) <= k:
                 max_size = max(max_size, end_idx - start_idx + 1)
                 end_idx += 1
             else:
-                counter[s[end_idx]] -= 1
                 counter[s[start_idx]] -= 1
                 start_idx += 1
+                end_idx += 1
 
         return max_size
