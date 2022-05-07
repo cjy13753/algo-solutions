916306e6d514c80cc809a453aa6477d6c7858fda
JunYoung Choi <cjy13753@gmail.com>
2022-05-07 09:46:55 +0900

    PASS: 2nd attempt with priority queue

--- leetcode/solution_23.py
+++ leetcode/solution_23.py
@@ -1,14 +1,16 @@
 '''
-Summary - Attempt #1 FAIL
+Summary - Attempt #2 PASS
 
 Your own answer? Yes
-Time spent: 50m
-Fail reason: Time Limit Exceeded (passed only leetcode example testcases)
-Time Complexity: O(n * m), where n is the number of list in lists and m is the length of a list with the most number of nodes.
+Time Complexity: O(nlogn) where n is the number of all nodes from lists. Visiting each node takes O(n) and heappush and heappop take O(nlogn).
 Space Complexity: O(1)
+
+Runtime: 104 ms, faster than 90.41% of Python3 online submissions for Merge k Sorted Lists.
+Memory Usage: 18 MB, less than 44.93% of Python3 online submissions for Merge k Sorted Lists.
 '''
 
 from typing import List, Optional
+import heapq
 
 # Definition for singly-linked list.
 class ListNode:
@@ -16,43 +18,27 @@ class ListNode:
         self.val = val
         self.next = next
 
-
 class Solution:
     def mergeKLists(self, lists: List[Optional[ListNode]]) -> Optional[ListNode]:
-        ans = None
-        ansTail = None
-        isFinished = False
-        length = len(lists)
+        dummy = ListNode()
+        tail = dummy
 
-        while isFinished is False:
-            minIdx = None
-            minNode = None
-            count = 0
-            for i in range(length):
-                if lists[i] is None:
-                    continue
-                count += 1
-                if minIdx == None:
-                    minIdx = i
-                    minNode = lists[i]
-                else:
-                    if lists[i].val < minNode.val:
-                        minIdx = i
-                        minNode = lists[i]
+        minHeap = []
+        for idx in range(len(lists)):
+            if lists[idx] is not None:
+                heapq.heappush(minHeap, (lists[idx].val, idx))
 
-            if count == 0:
-                isFinished = True
-            else:
-                if ans is None:
-                    ans = ListNode(lists[minIdx].val)
-                    ansTail = ans
-                else:
-                    ansTail.next = ListNode(lists[minIdx].val)
-                    ansTail = ansTail.next
-                lists[minIdx] = lists[minIdx].next
-                
+        while minHeap:
+            _, idx = heapq.heappop(minHeap)
+            tail.next = lists[idx]
+            lists[idx] = lists[idx].next
+            tail = tail.next
+            tail.next = None
+            
+            if lists[idx] is not None:
+                heapq.heappush(minHeap, (lists[idx].val, idx))
 
-        return ans
+        return dummy.next
 
 solution = Solution()
 
