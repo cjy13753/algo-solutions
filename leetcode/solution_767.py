425031a5ba030d763f14f32e1469b166c7a74f36
JunYoung Choi <cjy13753@gmail.com>
2022-05-13 12:45:26 +0900

    PASS: leetcode 767 using priority queue

--- leetcode/solution_767.py
+++ leetcode/solution_767.py
@@ -1,42 +1,51 @@
 '''
-Summary - Attempt #1 FAIL
+Summary
 
-Difficulty: Leetcode Medium
-Time spent: 50m
-FAIL type: Wrong Answer
-FAIL reason: started off with completely wrong idea
+Leetcode 767. Reorganize String (Medium)
+
+Attempt #2: PASS
+Your unique answer?: No
+Idea from: https://leetcode.com/problems/reorganize-string/discuss/113457/Simple-python-solution-using-PriorityQueue
+
+Time Complexity: O(nlogn) due to using heap tree, where n is the length of the given string
+Space Complexity: O(n) to hold each character and its frequency in the given string
+Runtime: 61 ms, faster than 21.61% of Python3 online submissions for Reorganize String.
+Memory Usage: 13.9 MB, less than 28.94% of Python3 online submissions for Reorganize String.
 '''
 
-from collections import defaultdict
+from collections import Counter
+import heapq
 
 class Solution:
     def reorganizeString(self, s: str) -> str:
         ans = ""
-        counter = defaultdict(int)
-        for c in s:
-            counter[c] += 1
+        container = []
+        for character, freq in Counter(s).items():
+            container.append((-freq, character))
+        maxHeap = []
+        for item in container:
+            heapq.heappush(maxHeap, item)
+        
+        temp_char = ''
+        temp_freq = 0
 
-        counterTuple = list(counter.items())
-        counterTuple.sort(key=lambda x: -x[1])
-        counterList = []
-        for e in counterTuple:
-            counterList.append(list(e))
+        while maxHeap:
+            item = heapq.heappop(maxHeap)
+            freq, character = -item[0], item[1]
+            ans += character
+            freq -= 1
+            if temp_freq != 0:
+                heapq.heappush(maxHeap, (temp_freq, temp_char))
+            temp_char = character
+            temp_freq = -(freq)
 
-        if len(s) == 1:
-            return s
-        elif len(counterList) == 1:
-            return ans
-        elif counterList[0][1] - counterList[1][1] > 1:
-            return ans
+        if temp_freq != 0:
+            return ""
         else:
-            for i in range(counterList[0][1]):
-                for i in range(len(counterList)):
-                    if counterList[i][1] != 0:
-                        ans += counterList[i][0]
-                        counterList[i][1] -= 1
             return ans
+        
 
-testcases = ["ccb", "cccb"]
+testcases = ["ccb", "cccb", "vvvlo"]
 solution = Solution()
 for testcase in testcases:
     print(solution.reorganizeString(testcase))
