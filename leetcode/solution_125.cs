03de0d3e3a616c928d3a90785b523ba3515244d1
Junyoung Choi (최준영) <cjy13753@gmail.com>
2022-09-13 10:05:36 +0900

    leetcode 125, TC:O(n), SC:O(1)

--- leetcode/solution_125.cs
+++ leetcode/solution_125.cs
@@ -1,26 +1,47 @@
-// Time Complexity: O(n) where cleaning the string takes O(n) and comparing forward with backward takes O(n)
-// Runtime: 142 ms, faster than 31.54% of C# online submissions for Valid Palindrome.
+// Time Complexity: O(n)
+// Runtime: 139 ms, faster than 34.57% of C# online submissions for Valid Palindrome.
 
-// Space Complexity: O(n) due to using string builder.
-// Memory Usage: 38.6 MB, less than 73.19% of C# online submissions for Valid Palindrome.
+// Space Complexity: O(1)
+// Memory Usage: 38.3 MB, less than 94.84% of C# online submissions for Valid Palindrome.
 
 public class Solution {
     public bool IsPalindrome(string s) {
-        
-        var cleanS = CleanseString(s);
-        Console.WriteLine(cleanS);
-        
-        if (cleanS.Length < 2)
+        if (s.Length < 2)
         {
             return true;
         }
         
         var forward = 0;
-        var backward = cleanS.Length - 1;
+        var backward = s.Length - 1;
         
         while (forward < backward)
         {
-            if (cleanS[forward] != cleanS[backward])
+            var fLetter = s[forward];
+            var bLetter = s[backward];
+            
+            if (!Char.IsLetterOrDigit(fLetter))
+            {
+                forward++;
+                continue;
+            }
+            
+            if (!Char.IsLetterOrDigit(bLetter))
+            {
+                backward--;
+                continue;
+            }
+            
+            if (Char.IsLetter(fLetter))
+            {
+                fLetter = Char.ToLower(fLetter);
+            }
+            
+            if (Char.IsLetter(bLetter))
+            {
+                bLetter = Char.ToLower(bLetter);
+            }
+            
+            if (fLetter != bLetter)
             {
                 return false;
             }
@@ -30,34 +51,5 @@ public class Solution {
         }
         
         return true;
-    }
-    
-    private string CleanseString(string s)
-    {
-        var sb = new StringBuilder();
-        
-        foreach (var c in s)
-        {
-            if (!Char.IsLetterOrDigit(c))
-            {
-                continue;
-            }
-            
-            if (Char.IsNumber(c))
-            {
-                sb.Append(c);
-                continue;
-            }
-            
-            if (Char.IsUpper(c))
-            {
-                sb.Append(Char.ToLower(c));
-                continue;
-            }
-            
-            sb.Append(c);
-        }
-        
-        return sb.ToString();
     }
 }
