b7ff207e753e262355c848277d548e5c57ceca5c
Junyoung Choi (최준영) <cjy13753@gmail.com>
2022-11-04 23:24:04 +0900

    leetcode 15, fail to come up with the solution

---
+++ leetcode/solution_15.cs
@@ -0,0 +1,16 @@
+// Coun't come up with the solution
+
+
+/*
+brute force approach would have TC O(n^3), so don't even consider as a viable option
+-4, -1, -1, 0, 1, 2
+can never think of possible approach.
+I thought hardly abouy ways to simplify the problem to one about finding tuplit that sum up to the other number fixed for each time. But they all seem to be TC O(n^3)
+
+*/
+
+public class Solution {
+    public IList<IList<int>> ThreeSum(int[] nums) {
+        
+    }
+}
