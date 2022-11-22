085be513f392de852c8f8ab7167bb2a159c04064
Junyoung Choi (최준영) <cjy13753@gmail.com>
2022-11-22 20:18:03 +0900

    leetcode 15, TC: O(n^2), SC: O(1)

--- leetcode/solution_15.cs
+++ leetcode/solution_15.cs
@@ -1,16 +1,71 @@
-// Coun't come up with the solution
-
-
-/*
-brute force approach would have TC O(n^3), so don't even consider as a viable option
--4, -1, -1, 0, 1, 2
-can never think of possible approach.
-I thought hardly abouy ways to simplify the problem to one about finding tuplit that sum up to the other number fixed for each time. But they all seem to be TC O(n^3)
-
-*/
+// Time Complexity: O(n^2) where n is the number of elements in the given array
+// Space Complexity: O(1) if not accounting for the output list
 
 public class Solution {
     public IList<IList<int>> ThreeSum(int[] nums) {
+        Array.Sort(nums);
+        var res = new List<IList<int>>();
+        
+        var prevNum = int.MinValue;
+        var i = 0;
+        
+        while (i < nums.Length)
+        {
+            if (nums[i] == prevNum)
+            {
+                i++;
+                continue;
+            }
+            
+            prevNum = nums[i];
+            
+            var target = (-1) * nums[i];
+            var start = i + 1;
+            var end = nums.Length - 1;
+            
+            while (start < end)
+            {
+                var twoSum = nums[start] + nums[end];
+                if (twoSum > target)
+                {
+                    end--;
+                }
+                else if (twoSum == target)
+                {
+                    res.Add(new List<int>{nums[i], nums[start], nums[end]});
+                    var prevStart = nums[start];
+                    var prevEnd = nums[end];
+                    start++;
+                    end--;
+                    
+                    while (start < nums.Length)
+                    {
+                        if (nums[start] == prevStart)
+                        {
+                            start++;
+                            continue;
+                        }
+                        break;
+                    }
+                    
+                    while (end > start)
+                    {
+                        if (nums[end] == prevEnd)
+                        {
+                            end--;
+                            continue;
+                        }
+                        break;
+                    }
+                }
+                else
+                {
+                    start++;
+                }
+            }
+        }
+        
+        return res;
         
     }
 }
