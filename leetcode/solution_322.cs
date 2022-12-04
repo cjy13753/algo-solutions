85ec9ae2e453d67ffb424b2c68739ec6eec44564
Junyoung Choi (최준영) <cjy13753@gmail.com>
2022-12-04 22:13:07 +0900

    leetcode 322, TC: O(n), SC: O(n)

--- leetcode/solution_322.cs
+++ leetcode/solution_322.cs
@@ -1,54 +1,59 @@
-/*
+// Time Complexity: O(n) where n is the input parameter, amount
+// Space Complexity: O(n) where n is the input parameter, amount
 
-sort coins in the ascending manner.
-discard elements that are bigger than amount in the sorted array.
-create a dictionary to hold calculated value;
+/*
+amount = 11
+min(dp[10], dp[9], dp[6]) + 1
+dp[0] = 0
+dp[1] = min(dp[0], dp[-1], dp[-4]) + 1 <= if all the indices are negative integer or all dp values are -1, just put -1 in the array for the index 1
+dp[2] = 
+dp[3] = 
+dp[4] = 
+dp[5] = 
+dp[6] = 
 */
 
 public class Solution {
     public int CoinChange(int[] coins, int amount) {
-        Array.Sort(coins);
-        var newCoins = coins.Where((val, idx) => val <= 10000).ToArray();
-        var dictionary = new Dictionary<int, int>();
-        var result = recursive(newCoins, amount, dictionary);
-        return result == -1 ? -1 : result - 1;
-    }
-    
-    private int recursive(int[] coins, int n, Dictionary<int, int> dictionary)
-    {
-        if (n < 0)
+        var dp = new int[amount + 1];
+        dp[0] = 0;
+        
+        for (int i = 1; i <= amount; i++)
         {
-         return -1;
-        }
-        if (n == 0)
-        {
-            return 1;
+            var indexCandidates = new List<int>();
+            foreach (var coin in coins)
+            {
+                if (i - coin >= 0)
+                {
+                    indexCandidates.Add(i - coin);
+                }
+            }
+            
+            if (indexCandidates.Count == 0)
+            {
+                dp[i] = -1;
+                continue;
+            }
+            
+            var dpCandidates = new List<int>();
+            foreach (var candidate in indexCandidates)
+            {
+                if (dp[candidate] != -1)
+                {
+                    dpCandidates.Add(dp[candidate]);
+                }
+            }
+            
+            if (dpCandidates.Count == 0)
+            {
+                dp[i] = -1;
+                continue;
+            }
+            
+            dp[i] = dpCandidates.Min() + 1;
         }
         
-        if (dictionary.ContainsKey(n))
-            return dictionary[n];
-        var minValue = int.MaxValue;
-        var flag = false;
-        foreach (var e in coins)
-        {
-            var val = recursive(coins, n-e, dictionary);
-            if (val != -1)
-            {
-                val++;
-                minValue = Math.Min(minValue, val);
-                flag = true;
-            }
-        }
-            
-            
-        if (flag == true)
-        {
-            dictionary[n] = minValue;
-            return minValue;
-        }
-        else
-        {
-            return -1;
-        }
+        
+        return dp[amount];
     }
 }
