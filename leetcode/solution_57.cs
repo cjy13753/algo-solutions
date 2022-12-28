10a416637d6abf6bb1971cbdcbc5b005ffde6818
Junyoung Choi (최준영) <cjy13753@gmail.com>
2022-12-28 10:10:39 +0900

    leetcode 57, TC: O(n), SC:O(1)

--- leetcode/solution_57.cs
+++ leetcode/solution_57.cs
@@ -1,60 +1,46 @@
-// Fail to solve
-
-/*
-
-The core concept of the approach to this problem is to not insert the new interval until when it's certain to insert the new interval and how it should be merged with neighbors.
-
-
-if newInterval end < interval start
-     put newInterval and, put the rest
-     return res
-else
-    merged's left is min of newInterval left and iteration's left
-
-if newInterval start > interval's end
-    just put the interval in the res
-
-else
-    end merg's right is max of newInterval's end and iterations'right
-
-put the last in the res
-
-return
-
-
-
-*/
-
+// Time Complexity: O(n) when n is the number of elments in the given array
+// Space Complexity: O(1) if not counting result array
 public class Solution {
     public int[][] Insert(int[][] intervals, int[] newInterval) {
-        var res = new List<int[]>();
-        var start = 0;
-        
-        for (int i = 0; i < intervals.Length; i++)
+        var result = new List<int[]>();
+        var inserted = false;
+
+        foreach (var oldInterval in intervals)
         {
-            var interval = intervals[i];
-            
-            if (newInterval[1] < interval[0])
+            var oldHead = oldInterval[0];
+            var oldTail = oldInterval[1];
+            var newHead = newInterval[0];
+            var newTail = newInterval[1];
+
+            if (newTail < oldHead)
             {
-                res.Add(newInterval);
-                if (i < intervals.Length - 1)
+                if (inserted)
                 {
-                    var rest = intervals[i..].Select(e => (int[])e.Clone()).ToArray();
-                    res.Add(rest);
+                    result.Add(new int[]{ oldHead, oldTail });
                 }
-                return res;
+                else
+                {
+                    result.Add(new int[] { newHead, newTail });
+                    result.Add(new int[]{ oldHead, oldTail });
+                    inserted = true;
+                }
             }
-            else if (interval[1] < newInterval[0])
+            else if (newTail >= oldHead && newHead <= oldTail)
             {
-                res.Add((int[])interval.Clone());
+                newInterval[0] = Math.Min(oldHead, newHead);
+                newInterval[1] = Math.Max(oldTail, newTail);
             }
             else
             {
-                start = Math.Min(interval[0], newInterval[0]);
+                result.Add(new int[] { oldHead, oldTail });
             }
         }
+
+        if (!inserted)
+        {
+            result.Add(new int[] { newInterval[0], newInterval[1] });
+        }
         
-        res.Add(new int[]{start, });
-        
-        return res.ToArray();
+        return result.ToArray();
+    }
 }
